37275ae96e318d40c3a46aaad464f8ff0bd890be
Yoan Malaver <yoanmalaver@gmail.com>
2022-12-29 18:04:53 -0500

    Add validation Age form

--- Project-Shopos-Bank/Bank_fe/src/app/components/create-client/create-client.component.ts
+++ Project-Shopos-Bank/Bank_fe/src/app/components/create-client/create-client.component.ts
@@ -11,11 +11,23 @@ import { ClientService } from 'src/app/shared/services/client.service';
 export class CreateClientComponent {
   title: string = 'Registro Clientes';
   newClient: Client = new Client();
+  edad!: number;
   constructor(private _clientService: ClientService, private _router: Router) {}
 
+  ageCalculator() {
+    if (this.newClient.dateOfBirth) {
+      const covertAge = new Date(this.newClient.dateOfBirth);
+      const timeDiff = Math.abs(Date.now() - covertAge.getTime());
+      this.edad = Math.floor(timeDiff / (1000 * 3600 * 24) / 365);
+    }
+  }
   addClient(): void {
-    this._clientService.createClient(this.newClient).subscribe((res) => {
-      this._router.navigate(['view-clients']);
-    });
+    this.ageCalculator();
+    console.log(this.edad);
+    if (this.edad >= 18) {
+      this._clientService.createClient(this.newClient).subscribe((res) => {
+        this._router.navigate(['view-clients']);
+      });
+    }
   }
 }
